6c4f5e1a78463854b9833e865b6da253302a1dd2
ios_jeet <ios.jeet@gmail.com>
2017-02-18 23:02:47 +0530

    Commit with new Source file

---
+++ SGPod/Classes/CustomButton.swift
@@ -0,0 +1,21 @@
+//
+//  CustomButton.swift
+//  SGPod
+//
+//  Created by Apple on 2/18/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class CustomButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
